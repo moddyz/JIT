a62444899559331dddeeff04963e7ab2ed431ef2
moddyz <moddy3d@gmail.com>
2020-02-23 22:39:39 +1300

    Added CMake files

--- jit/jit.cpp
+++ jit/jit.cpp
@@ -2,10 +2,10 @@
 
 namespace jit
 {
-JIT::JIT( llvm::JITTargetMachineBuilder JTMB, llvm::DataLayout m_dataLayout )
+JIT::JIT( llvm::JITTargetMachineBuilder i_targetMachineBuilder, llvm::DataLayout i_dataLayout )
     : m_objectLayer( m_executionSession, []() { return std::make_unique< llvm::SectionMemoryManager >(); } )
-    , m_compileLayer( m_executionSession, m_objectLayer, ConcurrentIRCompiler( std::move( JTMB ) ) )
-    , m_dataLayout( std::move( m_dataLayout ) )
+    , m_compileLayer( m_executionSession, m_objectLayer, ConcurrentIRCompiler( std::move( i_targetMachineBuilder ) ) )
+    , m_dataLayout( std::move( i_dataLayout ) )
     , m_mangler( m_executionSession, this->m_dataLayout )
     , m_context( std::make_unique< LLVMContext >() )
 {
--- jit/jit.h
+++ jit/jit.h
@@ -19,16 +19,7 @@ namespace jit
 class JIT
 {
 public:
-    explicit JIT( llvm::JITTargetMachineBuilder JTMB, llvm::DataLayout m_dataLayout );
-        : m_objectLayer( m_executionSession, []() { return std::make_unique< llvm::SectionMemoryManager >(); } )
-        , m_compileLayer( m_executionSession, m_objectLayer, ConcurrentIRCompiler( std::move( JTMB ) ) )
-        , m_dataLayout( std::move( m_dataLayout ) )
-        , m_mangler( m_executionSession, this->m_dataLayout )
-        , m_context( std::make_unique< LLVMContext >() )
-    {
-        m_executionSession.getMainJITDylib().setGenerator(
-            cantFail( DynamicLibrarySearchGenerator::GetForCurrentProcess( m_dataLayout ) ) );
-    }
+    explicit JIT( llvm::JITTargetMachineBuilder i_targetMachineBuilder, llvm::DataLayout i_dataLayout );
 
 private:
     llvm::ExecutionSession         m_executionSession;
