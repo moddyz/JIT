3e0d4707f2350b4f2767ff60ec836254ada41ef1
moddyz <moddy3d@gmail.com>
2020-12-20 16:21:55 +1300

    FindNamedClass to take class name and file path as args.

--- src/findNamedClass/main.cpp
+++ src/findNamedClass/main.cpp
@@ -1,23 +1,31 @@
+/// findNamedClass: Simple utility for finding and printing occurrances of
+/// a specified class name in a given source file.
+
 #include "clang/AST/ASTConsumer.h"
 #include "clang/AST/RecursiveASTVisitor.h"
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/FrontendAction.h"
 #include "clang/Tooling/Tooling.h"
 
+#include <fstream>
+
 using namespace clang;
 
 class FindNamedClassVisitor : public RecursiveASTVisitor<FindNamedClassVisitor>
 {
 public:
-    explicit FindNamedClassVisitor(ASTContext* context)
-      : context(context)
+    explicit FindNamedClassVisitor(const char* className, ASTContext* context)
+      : _className(className)
+      , _context(context)
     {}
 
     bool VisitCXXRecordDecl(CXXRecordDecl* Declaration)
     {
-        if (Declaration->getQualifiedNameAsString() == "n::m::C") {
+        llvm::outs() << "Checking: " << Declaration->getQualifiedNameAsString()
+                     << "\n";
+        if (Declaration->getQualifiedNameAsString() == _className) {
             FullSourceLoc fullLocation =
-                context->getFullLoc(Declaration->getBeginLoc());
+                _context->getFullLoc(Declaration->getBeginLoc());
             if (fullLocation.isValid())
                 llvm::outs() << "Found declaration at "
                              << fullLocation.getSpellingLineNumber() << ":"
@@ -27,42 +35,68 @@ public:
     }
 
 private:
-    ASTContext* context;
+    ASTContext* _context = nullptr;
+    const char* _className = nullptr;
 };
 
 class FindNamedClassConsumer : public clang::ASTConsumer
 {
 public:
-    explicit FindNamedClassConsumer(ASTContext* context)
-      : visitor(context)
+    explicit FindNamedClassConsumer(const char* className, ASTContext* context)
+      : _visitor(className, context)
     {}
 
     virtual void HandleTranslationUnit(clang::ASTContext& context)
     {
-        visitor.TraverseDecl(context.getTranslationUnitDecl());
+        _visitor.TraverseDecl(context.getTranslationUnitDecl());
     }
 
 private:
-    FindNamedClassVisitor visitor;
+    FindNamedClassVisitor _visitor;
 };
 
 class FindNamedClassAction : public clang::ASTFrontendAction
 {
 public:
+    explicit FindNamedClassAction(const char* className)
+      : _className(className)
+    {}
+
     virtual std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(
         clang::CompilerInstance& Compiler,
         llvm::StringRef InFile)
     {
         return std::unique_ptr<clang::ASTConsumer>(
-            new FindNamedClassConsumer(&Compiler.getASTContext()));
+            new FindNamedClassConsumer(_className, &Compiler.getASTContext()));
     }
+
+private:
+    const char* _className = nullptr;
 };
 
 int
 main(int argc, char** argv)
 {
-    if (argc > 1) {
-        clang::tooling::runToolOnCode(std::make_unique<FindNamedClassAction>(),
-                                      argv[1]);
+    if (argc != 3) {
+        llvm::outs() << "usage: findNamedClass <CLASS_NAME> <FILE>\n";
+        return EXIT_FAILURE;
     }
+
+    // Open file.
+    std::ifstream inputFile(argv[2]);
+    std::string code;
+
+    // Pre-allocate code string size.
+    inputFile.seekg(0, std::ios::end);
+    code.reserve(inputFile.tellg());
+    inputFile.seekg(0, std::ios::beg);
+
+    // Copy entire contents of file into code.
+    code.assign((std::istreambuf_iterator<char>(inputFile)),
+                std::istreambuf_iterator<char>());
+
+    clang::tooling::runToolOnCode(
+        std::make_unique<FindNamedClassAction>(argv[1]), code.c_str());
+
+    return EXIT_SUCCESS;
 }
